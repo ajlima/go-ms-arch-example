d86a9a419ef55c6e1c02c1c5a5686ddc9b4f25b1
Alexandre Lima <alexandre.lima@penguin-success.com>
2021-12-12 03:07:40 +0000

    Update stress parameters

--- stress/stress.sh
+++ stress/stress.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-ab -n 100000 -c 200 -H "Accept:application/json" -k -p ./sale.json -T "application/json" http://localhost:8080/register/sale/
+ab -n 1000000 -c 250 -H "Accept:application/json" -k -p ./sale.json -T "application/json" http://localhost:8081/api/v1/register/sale/
